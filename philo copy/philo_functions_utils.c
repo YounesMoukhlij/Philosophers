92f3a9dcf3af2611bc2b9653162d979ac89a11cd
Younes Moukhlij <youmoukh@e3r7p7.1337.ma>
2024-02-20 18:06:31 +0100

    asd

---
+++ philo copy/philo_functions_utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_functions_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 15:38:02 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/02/19 12:33:10 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	lonely_philo(t_philo *philo)
+{
+	print_func(philo, "has taken a fork");
+}
+
+int	ft_atoi(char *s)
+{
+	long	r;
+	int		sign;
+
+	r = 0;
+	sign = 1;
+	while (*s && (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
+	while (*s && (*s >= '0' && *s <= '9'))
+	{
+		r = r * 10 + *s - 48;
+		if (r > INT_MAX || r < INT_MIN)
+			error_parsing(1);
+		s++;
+	}
+	return (r * sign);
+}
+
+long long	what_time_now(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+void	time_between_taches(long long time, t_program *prg)
+{
+	long long	i;
+
+	i = what_time_now();
+	while (!(prg->dead))
+	{
+		if (what_time_now() - i >= time)
+			break ;
+		usleep(50);
+	}
+}
+
+void	print_func(t_philo *prg, char *str)
+{
+	t_program	*p;
+
+	p = prg->philos_infos;
+	pthread_mutex_lock(&(p->print_habbit));
+	if (!(p->dead))
+		printf("%lld %d %s\n", (what_time_now() - p->time_start), \
+		prg->philo_id, str);
+	pthread_mutex_unlock(&p->print_habbit);
+}
---
+++ philo copy/philosophers.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 15:29:50 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/02/20 17:38:55 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	show(void)
+{
+	system("leaks philo");
+}
+
+int	main(int ac, char **av)
+{
+	t_program	prg;
+
+	ft_parse_param(ac, av);
+	init_all_infos(&prg, av);
+	init_mutex(&prg);
+	wake_up_philosophers(&prg);
+	philos_life(&prg);
+	check_if_some_philo_is_dead(&prg, prg.philos);
+	free_all_philos(&prg, prg.philos);
+	return (0);
+}
